c5cbef1f6d2cd8867f27ac72dfbf900800cb3fba
GueyLing <glhneah@gmail.com>
2024-06-07 21:36:20 +0800

    update cors

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const cors = require('cors');
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
