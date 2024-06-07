c368bf9557265af6b53eb41c7977ba9e538906e9
GueyLing <glhneah@gmail.com>
2024-06-07 22:25:07 +0800

    back

--- index.js
+++ index.js
@@ -9,14 +9,7 @@ const cors = require('cors');
 const app = express();
 
 // middleware
-app.use(cors({
-    origin: 'http://localhost:4200', // Adjust this as needed
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  }));
-  
-  // Handle preflight requests
-  app.options('*', cors());
+app.use(cors());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
